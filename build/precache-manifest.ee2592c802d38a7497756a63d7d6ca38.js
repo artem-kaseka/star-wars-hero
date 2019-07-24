1a55260610568e4e6822f02e1ac470d776d035d1
Artem Kaseka <artem.kaseka@synapse.com>
2019-07-24 10:53:56 +0300

    added build version

---
+++ build/precache-manifest.ee2592c802d38a7497756a63d7d6ca38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea86b266ca0a0b9c051efdbce2dd510b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be68d27d6f847436567e",
+    "url": "/static/css/main.4754fa9f.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "be68d27d6f847436567e",
+    "url": "/static/js/main.1292efe8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fda207a969742c3573c3e6a793cc5c66",
+    "url": "/static/media/ou7Yofa.fda207a9.jpg"
+  }
+]);
