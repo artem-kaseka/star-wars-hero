3afed08ffd72eb54bef95635cd0e53db18253049
Artem Kaseka <artem.kaseka@synapse.com>
2019-07-24 11:17:41 +0300

    Updates

---
+++ precache-manifest.076e0f5836d3cc58112edf605c1be853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f328a789fc4abe6f684f8af014cb5e",
+    "url": "/star-wars-hero/index.html"
+  },
+  {
+    "revision": "ecd1aaabb30a5af524f3",
+    "url": "/star-wars-hero/static/css/main.2ad38ce8.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/star-wars-hero/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "ecd1aaabb30a5af524f3",
+    "url": "/star-wars-hero/static/js/main.1292efe8.chunk.js"
+  },
+  {
+    "revision": "9ea4d9489932d1924840",
+    "url": "/star-wars-hero/static/js/runtime~main.5b274742.js"
+  },
+  {
+    "revision": "fda207a969742c3573c3e6a793cc5c66",
+    "url": "/star-wars-hero/static/media/ou7Yofa.fda207a9.jpg"
+  }
+]);
